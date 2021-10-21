b94582e53675964cf6bab0fdd640e98a9fb52563
z3b0w <andryslovas@gmail.com>
2021-10-21 11:06:45 +0300

    front end js example

--- src/js/front.js
+++ src/js/front.js
@@ -1,1 +1,12 @@
 console.log('front.js');
+
+
+// select elements
+const submitBtnEl = document.getElementById('add-book')
+const categorySelectEl = document.getElementById('category')
+
+submitBtnEl.setAttribute('disabled', true);
+
+categorySelectEl.addEventListener('change', (e) => {
+    submitBtnEl.removeAttribute('disabled');
+})
